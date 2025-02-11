3cb86bd0aff1eaacf558cf739318bd6f58d8d6ad
Xavier Arbour <arbour.xavier34@gmail.com>
2025-02-11 16:07:17 -0500

    Ajout de Javascript
    Pour controler l'état actif/ inactif des boutons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-let boutons = document.querySelectorAll('');
+// On récupère les boutons permettant la navigation dans les différentes pages
+let boutons = document.querySelectorAll('.boutonNav>a');
+
+// On capture l'URL de la page actuelle
+let pageActuelle = window.location.pathname.replace(/^\//, '');
+
+// On fait une boucle pour voir l'URL de chaque bouton
+boutons.forEach(bouton => {
+    // On capture l'URL de chaque bouton
+    let url = bouton.getAttribute('href');
+
+    // Si l'URL du bouton est égale à l'URL de la page actuelle
+    if (url === pageActuelle) {
+        //
+        let parent = bouton.parentElement;
+        // On ajoute la classe 'active' au bouton
+        parent.classList.add('active');
+
+        // On change l'image du fond en fonction de la page
+        if (url == "chatPocalypseNow.html") 
+        {
+            parent.style.backgroundImage = "url(../images/movie_Jaune.png)"
+        }
+        else if (url == "creation.html")
+        {
+            parent.style.backgroundImage = "url(../images/icone_Prod_Actif.png)"
+        }
+        else if (url == "aPropos.html")
+        {
+            bouton.style.color = "#FEB312";
+        }
+    }
+});
