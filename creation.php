337b7b528f110def5d49150042919b78ede7c81c
Xavier Arbour <arbour.xavier34@gmail.com>
2025-02-25 12:03:47 -0500

    Changement des textes et ajout d'images

--- creation.php
+++ creation.php
@@ -4,95 +4,106 @@ Template Name: creation
 */
 ?>
 
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
-    <div class="wrapper-barres">
-        <div class="orange"></div>
-        <div class="bleu"></div>
+<div class="wrapper-barres">
+  <div class="orange"></div>
+  <div class="bleu"></div>
+</div>
+
+<div class="texte1 mt-3 pr-3">
+  <h1 class="h1-responsive">Chat-Pocalypse Now</h1>
+  <p class="crédit-responsive">Un Demo Reel par Xavier Arbour</p>
+</div>
+
+<div class="wrapper-img-head">
+  <div class="flou-top-img"></div>
+  <img class="chatHead" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat2.jpg" alt="image chat">
+</div>
+<!-- HEADER -->
+
+<!-- CONTENU -->
+<div class="wrapper-flou-body px-3">
+  <div class="flou-milieu-body">
+  </div>
+</div>
+
+<div class="contenu texte-intro">
+  <h2 class="mb-4 mx-2">Processus de création :</h2>
+  <p class="mb-5">La création de ce chat 3D a débuté par une recherche approfondie d’images de référence, permettant de mieux comprendre l’anatomie et les proportions de l'animal. <br><br>
+
+    Ensuite, le modèle a été sculpté dans ZBrush avec un souci du réalisme avant d’être exporté en format FBX vers Maya pour la suite du processus.<br>
+
+    Dans Maya, j’ai procédé au dépliage des UVs afin d’appliquer des textures de base, notamment une texture transparente pour le globe oculaire ainsi qu'une texture simple pour l'iris du chat et une matériel de base pour la peau.<br><br>
+
+    Après cela, j’ai réalisé le rigging et le skinning du modèle pour permettre son animation. Avant d’aller plus loin, j’ai testé l’animation afin de m’assurer de la bonne déformation du modèle.<br><br>
+
+    Une fois ces étapes validées, j’ai utilisé XGen pour créer un grooming le plus réaliste possible, avec mes limitations graphiques, ajoutant du volume et du détail au pelage du chat.<br><br>
+
+    Enfin, j’ai procédé aux rendus finaux, mettant en valeur le modèle avec un éclairage dynamique et un décor simple.</p>
+
+  <!-- Carousel Bootstrap	 -->
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/25.02.2025_11.43.51_REC.png" alt="Premiere slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/25.02.2025_11.44.39_REC.png" alt="Deuxieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/25.02.2025_11.45.07_REC.png" alt="Troisieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatMaya_2.png" alt="Quatrieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatMaya_1.png" alt="Cinquieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Maya3.png" alt="Sixieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/25.02.2025_11.46.12_REC.png" alt="Septieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatTestRender.jpg" alt="Huitieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/25.02.2025_11.45.39_REC.png" alt="Neufieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Groom.0002_1.0001.jpg" alt="Dixieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_AnimTest1.0006_1.0001.jpg" alt="Onzieme slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Render1.jpg" alt="Douzieme slide">
+      </div>
     </div>
 
-    <div class="texte1 mt-3 pr-3">
-        <h1 class="h1-responsive">Chat-Pocalypse Now</h1>
-        <p class="crédit-responsive">Un Demo Reel par Xavier Arbour</p>
-    </div>
+    <!-- Boutons précédent/ suivant -->
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 
-    <div class="wrapper-img-head">
-        <div class="flou-top-img"></div>
-        <img class="chatHead" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat2.jpg" alt="image chat">
-    </div>
-    <!-- HEADER -->
+<!-- Vidéo -->
+<div class="contenu bck-film mt-1">
+  <div class="video p-3">
+    <iframe src="https://www.youtube.com/embed/6H8ZJNFKcec?si=E_DaEXMLn23P34Lq" title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+</div>
+<!-- CONTENU -->
 
-    <!-- CONTENU -->
-    <div class="wrapper-flou-body px-3">
-        <div class="flou-milieu-body">
-        </div>
-    </div>
-
-    <div class="contenu texte-intro">
-        <h2 class="mb-4 mx-2">Processus de création :</h2>
-        <!-- Style de l'image temporaire!!! -->
-        <p class="mb-5">La création de ce chat 3D a débuté par une recherche approfondie d’images de référence, permettant de mieux comprendre l’anatomie et les proportions de l'animal. <br><br>
-
-Ensuite, le modèle a été sculpté dans ZBrush avec un souci du réalisme avant d’être exporté en format FBX vers Maya pour la suite du processus.<br>
-
-Dans Maya, j’ai procédé au dépliage des UVs afin d’appliquer des textures de base, notamment une texture transparente pour le globe oculaire ainsi qu'une texture simple pour l'iris du chat et une matériel de base pour la peau.<br><br>
-
-Après cela, j’ai réalisé le rigging et le skinning du modèle pour permettre son animation. Avant d’aller plus loin, j’ai testé l’animation afin de m’assurer de la bonne déformation du modèle.<br><br>
-
-Une fois ces étapes validées, j’ai utilisé XGen pour créer un grooming le plus réaliste possible, avec mes limitations graphiques, ajoutant du volume et du détail au pelage du chat.<br><br>
-
-Enfin, j’ai procédé aux rendus finaux, mettant en valeur le modèle avec un éclairage dynamique et un décor simple.</p>
-		
-			<!-- Carousel Bootstrap	 -->
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/06.02.2025_10.33.59_REC.png" alt="Premiere slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/06.02.2025_10.34.27_REC.png" alt="Deuxieme slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatMaya_2.png" alt="Troisieme slide">
-                  </div>
-					           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatMaya_1.png" alt="Quatrieme slide">
-                  </div>
-					           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Maya3.png" alt="Cinquieme slide">
-                  </div>
-					           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chatTestRender.jpg" alt="Sixieme slide">
-                  </div>
-								           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Groom.0002_1.0001.jpg" alt="Septieme slide">
-                  </div>
-								           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_AnimTest1.0006_1.0001.jpg" alt="Huitieme slide">
-                  </div>
-								           <div class="carousel-item">
-                    <img class="d-block w-100" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat_Render1.jpg" alt="Neufieme slide">
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>
-    </div>
-
-    <div class="contenu bck-film mt-1">
-        <div class="video p-3">
-            <iframe src="https://www.youtube.com/embed/6H8ZJNFKcec?si=E_DaEXMLn23P34Lq" title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-    </div>
-    <!-- CONTENU -->
-
-    <?php get_footer(); ?> 
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,50 +1,50 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
-    <div class="wrapper-barres">
-        <div class="orange"></div>
-        <div class="bleu"></div>
+<div class="wrapper-barres">
+    <div class="orange"></div>
+    <div class="bleu"></div>
+</div>
+
+<div class="texte1 mt-3 pr-3">
+    <h1 class="h1-responsive">Chat-Pocalypse Now</h1>
+    <p class="crédit-responsive">Un Demo Reel par Xavier Arbour</p>
+</div>
+
+<div class="wrapper-img-head">
+    <div class="flou-top-img"></div>
+    <img class="chatHead" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat2.jpg" alt="image chat">
+
+</div>
+<!-- HEADER -->
+
+<!-- CONTENU -->
+<div class="wrapper-flou-body px-3">
+    <div class="flou-milieu-body">
     </div>
+</div>
 
-    <div class="texte1 mt-3 pr-3">
-        <h1 class="h1-responsive">Chat-Pocalypse Now</h1>
-        <p class="crédit-responsive">Un Demo Reel par Xavier Arbour</p>
+<div class="contenu texte-intro">
+    <h2 class="mb-4 mx-2">Le concept :</h2>
+    <!-- Style de l'image temporaire!!! -->
+    <img src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/Artboard-1.png" alt="" class="float-right imgshadow me-3 mb-3">
+    <p class="mb-5">Ce projet explore un workflow complet de création 3D, de la modélisation à l’animation.<br><br>
+
+        Le chat est d’abord sculpté dans ZBrush, en s’appuyant sur des références pour capturer son anatomie. Le modèle est ensuite exporté en FBX vers Maya, où ses UVs sont dépliées et des textures de base appliquées.<br><br>
+
+        Après cela, un rigging et un skinning sont réalisés pour permettre l’animation. Une fois les mouvements testés, le grooming est ajouté avec XGen pour un pelage le plus réaliste possible.<br><br>
+
+        Enfin, le modèle est rendu dans Maya, finalisant ainsi un processus qui intègre modélisation, rigging, animation et grooming dans un pipeline professionnel.
+    </p>
+</div>
+
+<div class="contenu bck-film mt-5">
+    <div class="video p-3">
+        <iframe src="https://www.youtube.com/embed/6H8ZJNFKcec?si=E_DaEXMLn23P34Lq" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
+</div>
+<!-- CONTENU -->
 
-    <div class="wrapper-img-head">
-        <div class="flou-top-img"></div>
-        <img class="chatHead" src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/chat2.jpg" alt="image chat">
-
-    </div>
-    <!-- HEADER -->
-
-    <!-- CONTENU -->
-    <div class="wrapper-flou-body px-3">
-        <div class="flou-milieu-body">
-        </div>
-    </div>
-
-    <div class="contenu texte-intro">
-        <h2 class="mb-4 mx-2">Le concept :</h2>
-        <!-- Style de l'image temporaire!!! -->
-        <img src="https://chatpocalypsenow.com/wp-content/uploads/2025/02/Artboard-1.png" alt="" class="float-right imgshadow me-3 mb-3">
-        <p>Pour mon projet final en intégration multimédia, j’ai modélisé et groomé un chat en 3D en visant une représentation la plus fidèle possible.
-			<br>
-
-J’ai sculpté la base dans ZBrush, optimisé la topologie et les UVs dans Maya, puis utilisé XGen pour le grooming. J’ai ajusté la densité, la longueur et l’orientation des poils pour un rendu cohérent.
-
-Ce projet m’a permis d’approfondir mes compétences en modélisation organique et en grooming, tout en respectant les contraintes techniques d’un pipeline 3D.
-
-</p>
-    </div>
-
-    <div class="contenu bck-film">
-        <div class="video p-3">
-            <iframe src="https://www.youtube.com/embed/6H8ZJNFKcec?si=E_DaEXMLn23P34Lq" title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-    </div>
-    <!-- CONTENU -->
-
-    <?php get_footer(); ?> 
+<?php get_footer(); ?>
