e6fea6a3751098152a84df6f5a6b14ffdf8a1e24
Xavier Arbour <arbour.xavier34@gmail.com>
2025-03-03 15:45:12 -0500

    Retrait des script tags qui sont superflux

--- footer.php
+++ footer.php
@@ -12,14 +12,6 @@
     <!-- Include Popper.js (requis pour Bootstrap) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
-    <!-- Include Bootstrap JavaScript -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-    <!-- Script pour Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-
     <!-- Include carousel.js de bootstrap -->
     <script src="<?php echo get_template_directory_uri(); ?>/sass/bootstrap-4.3.1/js/dist/util.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/sass/bootstrap-4.3.1/js/dist/carousel.js"></script>
